7e007f7472d0fb3aa0b0e54edb9b2bed130cbad0
Vojtech Rinik <vojto@rinik.net>
2014-01-14 22:17:08 +0100

    Fixing redis verification code

--- config/stack/redis.rb
+++ config/stack/redis.rb
@@ -2,8 +2,8 @@ package :redis do
   description 'Redis Database'
   apt 'redis-server'
   verify do
-    has_executable '/usr/local/bin/redis-server'
-    has_file '/usr/local/bin/redis-server'
+    has_executable '/usr/bin/redis-server'
+    has_file '/usr/bin/redis-server'
   end
 
   requires :build_essential
